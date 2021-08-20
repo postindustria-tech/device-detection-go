76a9ac01fa7a5919636420f144d78abd209ba6ce
Tung Pham <tung@51degrees.com>
2021-08-20 15:00:39 +0100

    EXAMPLE: Concrete implementation of 'Getting Started' example.

--- examples/onpremise/getting_started_test.go
+++ examples/onpremise/getting_started_test.go
@@ -1,4 +1,4 @@
-package main
+package ddonpremise_test
 
 /*
 To run this program, follow the following steps:
@@ -14,12 +14,38 @@ To run this program, follow the following steps:
 import "fmt"
 import "github.com/51Degrees/device-detection-go/ddonpremise"
 
+// function match performs a match on an input User-Agent string and determine
+// if the device is a mobile device.
+func match(
+	results ddonpremise.ResultsHash,
+	ua string) {
+	// Perform detection
+	err := ddonpremise.FiftyoneDegreesResultsHashFromUserAgent(
+		results,
+		ua)
+	if err != nil {
+		panic(err)
+	}
+
+	// Get the values in string
+	value, _, err := ddonpremise.FiftyoneDegreesResultsHashGetValuesString(
+		results,
+		"IsMobile",
+		100,
+		",")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("\tIsMobile: %s\n", value)
+}
+
 func Example() {
 	// Initialise manager
 	manager := ddonpremise.NewResourceManager()
 	props := ddonpremise.NewPropertiesRequired("")
 	config := ddonpremise.NewConfigHash()
-	filePath := "/home/fiftyonedegrees/staff/tung/device-detection-go/ddonpremise/device-detection-cxx/device-detection-data/51Degrees-LiteV4.1.hash"
+	filePath := "../../ddonpremise/device-detection-cxx/device-detection-data/51Degrees-LiteV4.1.hash"
 	err := ddonpremise.FiftyoneDegreesHashInitManagerFromFile(
 		manager,
 		config,
@@ -39,28 +65,31 @@ func Example() {
 		panic(err)
 	}
 
-	// Example User-Agent
-	ua := "Mozilla/5.0 (iPhone; CPU iPhone OS 7_1 like Mac OS X) AppleWebKit/537.51.2 (KHTML, like Gecko) Version/7.0 Mobile/11D167 Safari/9537.53"
+	// User-Agent string of an iPhone mobile device.
+	const uaMobile = "Mozilla/5.0 (iPhone; CPU iPhone OS 7_1 like Mac OS X) " +
+		"AppleWebKit/537.51.2 (KHTML, like Gecko) Version/7.0 Mobile/11D167 " +
+		"Safari/9537.53"
 
-	// Perform detection
-	err = ddonpremise.FiftyoneDegreesResultsHashFromUserAgent(
-		results,
-		ua)
-	if err != nil {
-		panic(err)
-	}
+	// User-Agent string of Firefox Web browser version 41 on desktop.
+	const uaDesktop = "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:41.0) " +
+		"Gecko/20100101 Firefox/41.0"
 
-	// Get the values in string
-	value, _, err := ddonpremise.FiftyoneDegreesResultsHashGetValuesString(
-		results,
-		"IsMobile",
-		100,
-		",")
-	if err != nil {
-		panic(err)
-	}
+	// User-Agent string of a MediaHub device.
+	const uaMediaHub = "Mozilla/5.0 (Linux; Android 4.4.2; X7 Quad Core " +
+		"Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 " +
+		"Chrome/30.0.0.0 Safari/537.36"
 
-	fmt.Println(value)
+	// Perform detection on mobile User-Agent
+	fmt.Printf("Mobile User-Agent: %s\n", uaMobile)
+	match(results, uaMobile)
+
+	// Perform detection on desktop User-Agent
+	fmt.Printf("\nDesktop User-Agent: %s\n", uaDesktop)
+	match(results, uaDesktop)
+
+	// Perform detection on MediaHub User-Agent
+	fmt.Printf("\nMediaHub User-Agent: %s\n", uaMediaHub)
+	match(results, uaMediaHub)
 
 	// Free allocated resources
 	err = ddonpremise.FiftyoneDegreesResultsHashFree(results)
@@ -74,5 +103,12 @@ func Example() {
 	}
 
 	// Output:
-	// True
+	// Mobile User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 7_1 like Mac OS X) AppleWebKit/537.51.2 (KHTML, like Gecko) Version/7.0 Mobile/11D167 Safari/9537.53
+	// 	IsMobile: True
+	//
+	// Desktop User-Agent: Mozilla/5.0 (Windows NT 6.3; WOW64; rv:41.0) Gecko/20100101 Firefox/41.0
+	// 	IsMobile: False
+	//
+	// MediaHub User-Agent: Mozilla/5.0 (Linux; Android 4.4.2; X7 Quad Core Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/30.0.0.0 Safari/537.36
+	// 	IsMobile: False
 }
