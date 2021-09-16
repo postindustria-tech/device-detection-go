15213113d1c79f4bca0b1b8255ccf094b8850551
Tung Pham (Admin) <tungadmin@51degreess-M1-Mac.local>
2021-09-16 12:51:40 +0100

    BUILD: With clag -latomic is not required so only include -latomic when not on Darwin. Build tags do not have option to check compiler so limit the build to Darwin and since we only test against clang on Darwin, this should be sufficient.

--- dd/cgoflags.go
+++ dd/cgoflags.go
@@ -5,5 +5,9 @@ package dd
 * centralize the specifications of these flags.
  */
 
-// #cgo LDFLAGS: -L./device-detection-cxx/build/lib -lfiftyone-hash-c -lfiftyone-device-detection-c -lfiftyone-common-c -lm -latomic
+/*
+#cgo CFLAGS: -fcommon
+#cgo LDFLAGS: -L./device-detection-cxx/build/lib -lfiftyone-hash-c -lfiftyone-device-detection-c -lfiftyone-common-c -lm
+#cgo !darwin LDFLAGS: -latomic
+*/
 import "C"
