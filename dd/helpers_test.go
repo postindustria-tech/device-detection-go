923a4de52ec4b8ac6752a2e30c8d2412530656da
Tung Pham <tung@51degrees.com>
2021-09-21 14:53:00 +0100

    REORGS/TEST: Added a helper file that contains helps functions for test. Refactored the results_hash_test.go to use decorator pattern so that instead of using TestMain to execute tests with different performance profiles, a decorator function is used instead. This way, the creation and freeing of the resource manager is done per performance profile for per test. Using TestMain causes the creation/freeing to be done for the whole test suite.

---
+++ dd/helpers_test.go
@@ -0,0 +1,16 @@
+package dd
+
+// TODO: Move the below to a sub package or common test file.
+// test User-Agents
+const uaMobile = "Mozilla/5.0 (iPhone; CPU iPhone OS 7_1 like Mac OS X) " +
+	"AppleWebKit/537.51.2 (KHTML, like Gecko) Version/7.0 Mobile/11D167 " +
+	"Safari/9537.53"
+
+// User-Agent string of Firefox Web browser version 41 on desktop.
+const uaDesktop = "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:41.0) " +
+	"Gecko/20100101 Firefox/41.0"
+
+// User-Agent string of a MediaHub device.
+const uaMediaHub = "Mozilla/5.0 (Linux; Android 4.4.2; X7 Quad Core " +
+	"Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 " +
+	"Chrome/30.0.0.0 Safari/537.36"
--- dd/results_hash_test.go
+++ dd/results_hash_test.go
@@ -2,30 +2,16 @@ package dd
 
 import (
 	"log"
-	"os"
 	"strings"
 	"testing"
 )
 
-// TODO: Move the below to a sub package or common test file.
-// test User-Agents
-const uaMobile = "Mozilla/5.0 (iPhone; CPU iPhone OS 7_1 like Mac OS X) " +
-	"AppleWebKit/537.51.2 (KHTML, like Gecko) Version/7.0 Mobile/11D167 " +
-	"Safari/9537.53"
+// Test function to be executed by a test decorator method
+type TestFunc func(manager *ResourceManager, t *testing.T)
 
-// User-Agent string of Firefox Web browser version 41 on desktop.
-const uaDesktop = "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:41.0) " +
-	"Gecko/20100101 Firefox/41.0"
-
-// User-Agent string of a MediaHub device.
-const uaMediaHub = "Mozilla/5.0 (Linux; Android 4.4.2; X7 Quad Core " +
-	"Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 " +
-	"Chrome/30.0.0.0 Safari/537.36"
-
-// Global manager to be used by each test
-var manager *ResourceManager = nil
-
-func TestMain(m *testing.M) {
+// This is a decorator method which take a test function and execute it with
+// different performance profile
+func testDifferentPerformanceProfiles(testFunc TestFunc, t *testing.T) {
 	// TODO: This should be replaced by a function that automatically search for
 	// correct file name.
 	// Get the data file Path
@@ -39,10 +25,9 @@ func TestMain(m *testing.M) {
 		InMemory,
 	}
 
-	code := 0
 	for _, perf := range data {
 		// Setup a resource manager
-		manager = NewResourceManager()
+		manager := NewResourceManager()
 		// Create config per performance
 		config := NewConfigHash(perf)
 		config.SetAllowUnmatched(false)
@@ -57,237 +42,251 @@ func TestMain(m *testing.M) {
 				dataFilePath)
 		}
 		// Run the test
-		code = m.Run()
+		testFunc(manager, t)
 
 		// Free the resource manager
 		manager.Free()
 		if err != nil {
 			log.Fatalln("Failed to free resource manager.")
 		}
-
-		// Exit if test failed
-		if code != 0 {
-			os.Exit(code)
-		}
 	}
-	os.Exit(code)
 }
 
 // This tests ResultsHash is created and freed
 // correctedly
 func TestResultsHashCreateFree(t *testing.T) {
-	// Create results hash
-	results, err := NewResultsHash(manager, 1, 0)
-	if err != nil {
-		t.Error("Failed to create results hash object.")
-	}
+	var testFunc TestFunc = func(manager *ResourceManager, t *testing.T) {
+		// Create results hash
+		results, err := NewResultsHash(manager, 1, 0)
+		if err != nil {
+			t.Error("Failed to create results hash object.")
+		}
 
-	// Free results hash
-	err = results.Free()
-	if err != nil {
-		t.Error("Failed to free results hash object.")
+		// Free results hash
+		err = results.Free()
+		if err != nil {
+			t.Error("Failed to free results hash object.")
+		}
 	}
+	// Execute the test with different performance profiles
+	testDifferentPerformanceProfiles(testFunc, t)
 }
 
 // This tests a detection is performed successfully on a
 // given User-Agent
 func TestMatchUserAgent(t *testing.T) {
-	data := []struct {
-		ua       string
-		expected string
-	}{
-		{uaMobile, "true"},
-		{uaDesktop, "false"},
-	}
+	var testFunc TestFunc = func(manager *ResourceManager, t *testing.T) {
+		data := []struct {
+			ua       string
+			expected string
+		}{
+			{uaMobile, "true"},
+			{uaDesktop, "false"},
+		}
 
-	// Create results hash
-	// Create results hash
-	results, err := NewResultsHash(manager, 1, 0)
-	if err != nil {
-		t.Error("Failed to create results hash object.")
-	}
-
-	// Free results hash at the end
-	defer func() {
-		err = results.Free()
+		// Create results hash
+		// Create results hash
+		results, err := NewResultsHash(manager, 1, 0)
 		if err != nil {
-			t.Error("Failed to free results hash object.")
-		}
-	}()
-
-	for _, testData := range data {
-		if err := results.MatchUserAgent(testData.ua); err != nil {
-			t.Errorf("Failed to perform detection on User-Agent \"%s\".\n",
-				testData.ua)
+			t.Error("Failed to create results hash object.")
 		}
 
-		// Get values
-		value, _, err := results.ValuesString("IsMobile", 50, ",")
-		if err != nil {
-			t.Errorf("Failed to get values string for User-Agent \"%s\".\n",
-				testData.ua)
-		}
-		// Check if detection is correct
-		if !strings.EqualFold(value, testData.expected) {
-			t.Errorf("Expected \"%s\" but get \"%s\"\n.",
-				testData.expected,
-				value,
-			)
+		// Free results hash at the end
+		defer func() {
+			err = results.Free()
+			if err != nil {
+				t.Error("Failed to free results hash object.")
+			}
+		}()
+
+		for _, testData := range data {
+			if err := results.MatchUserAgent(testData.ua); err != nil {
+				t.Errorf("Failed to perform detection on User-Agent \"%s\".\n",
+					testData.ua)
+			}
+
+			// Get values
+			value, _, err := results.ValuesString("IsMobile", 50, ",")
+			if err != nil {
+				t.Errorf("Failed to get values string for User-Agent \"%s\".\n",
+					testData.ua)
+			}
+			// Check if detection is correct
+			if !strings.EqualFold(value, testData.expected) {
+				t.Errorf("Expected \"%s\" but get \"%s\"\n.",
+					testData.expected,
+					value,
+				)
+			}
 		}
 	}
+	// Execute the test with different performance profiles
+	testDifferentPerformanceProfiles(testFunc, t)
 }
 
 // This tests match metrics are returned in correct
 // successfully and in correct format.
 func TestMatchMetrics(t *testing.T) {
-	// Create results hash
-	results, err := NewResultsHash(manager, 1, 0)
-	if err != nil {
-		t.Error("Failed to create results hash object.")
-	}
-
-	// Free results hash at the end
-	defer func() {
-		err = results.Free()
+	var testFunc TestFunc = func(manager *ResourceManager, t *testing.T) {
+		// Create results hash
+		results, err := NewResultsHash(manager, 1, 0)
 		if err != nil {
-			t.Error("Failed to free results hash object.")
+			t.Error("Failed to create results hash object.")
 		}
-	}()
 
-	if err := results.MatchUserAgent(uaMobile); err != nil {
-		t.Errorf("Failed to perform detection on User-Agent \"%s\".\n",
-			uaMobile)
-	}
+		// Free results hash at the end
+		defer func() {
+			err = results.Free()
+			if err != nil {
+				t.Error("Failed to free results hash object.")
+			}
+		}()
 
-	// Check iterations
-	if results.Iterations() <= 0 {
-		t.Error("Expected iterations to always be biggers than 0.")
-	}
+		if err := results.MatchUserAgent(uaMobile); err != nil {
+			t.Errorf("Failed to perform detection on User-Agent \"%s\".\n",
+				uaMobile)
+		}
 
-	// Check drift
-	if results.Drift() < 0 {
-		t.Error("Expected drift to always be bigger be than or equal to 0.")
-	}
+		// Check iterations
+		if results.Iterations() <= 0 {
+			t.Error("Expected iterations to always be biggers than 0.")
+		}
 
-	// Check difference
-	if results.Difference() < 0 {
-		t.Error("Expected difference to always be bigger than or equal to 0.")
-	}
+		// Check drift
+		if results.Drift() < 0 {
+			t.Error("Expected drift to always be bigger be than or equal to 0.")
+		}
 
-	// Check matched nodes
-	if results.MatchedNodes() <= 0 {
-		t.Error("Expected matched nodes to always bigger than 0.")
-	}
+		// Check difference
+		if results.Difference() < 0 {
+			t.Error("Expected difference to always be bigger than or equal to 0.")
+		}
 
-	// Check method
-	if results.Method() == None {
-		t.Error("Expected method to not be None.")
-	}
+		// Check matched nodes
+		if results.MatchedNodes() <= 0 {
+			t.Error("Expected matched nodes to always bigger than 0.")
+		}
 
-	// Check User-Agents
-	if results.UserAgents() != 1 {
-		t.Error("Expected number of used User-Agents to be 1 as there was " +
-			"only one input.")
-	}
+		// Check method
+		if results.Method() == None {
+			t.Error("Expected method to not be None.")
+		}
 
-	// Check match User-Agent string
-	if strings.TrimSpace(results.UserAgent(0)) == "" {
-		t.Error("Expected matched User-Agent not to be an empty or spacey " +
-			"string.")
+		// Check User-Agents
+		if results.UserAgents() != 1 {
+			t.Error("Expected number of used User-Agents to be 1 as there was " +
+				"only one input.")
+		}
+
+		// Check match User-Agent string
+		if strings.TrimSpace(results.UserAgent(0)) == "" {
+			t.Error("Expected matched User-Agent not to be an empty or spacey " +
+				"string.")
+		}
 	}
+	// Execute the test with different performance profiles
+	testDifferentPerformanceProfiles(testFunc, t)
 }
 
 // This tests HasValues reports correctly when a
 // value is expected to be returned
 func TestHasValues(t *testing.T) {
-	// Create results hash
-	results, err := NewResultsHash(manager, 1, 0)
-	if err != nil {
-		t.Error("Failed to create results hash object.")
-	}
-
-	// Free results hash at the end
-	defer func() {
-		err = results.Free()
+	var testFunc TestFunc = func(manager *ResourceManager, t *testing.T) {
+		// Create results hash
+		results, err := NewResultsHash(manager, 1, 0)
 		if err != nil {
-			t.Error("Failed to free results hash object.")
+			t.Error("Failed to create results hash object.")
 		}
-	}()
 
-	if err := results.MatchUserAgent(uaMobile); err != nil {
-		t.Errorf("Failed to perform detection on User-Agent \"%s\".\n",
-			uaMobile)
-	}
+		// Free results hash at the end
+		defer func() {
+			err = results.Free()
+			if err != nil {
+				t.Error("Failed to free results hash object.")
+			}
+		}()
 
-	// Check results
-	propertyName := "IsMobile"
-	hasValues, err := results.HasValues(propertyName)
-	if err != nil {
-		t.Error("Failed to perform 'HasValues' on results.")
-	}
+		if err := results.MatchUserAgent(uaMobile); err != nil {
+			t.Errorf("Failed to perform detection on User-Agent \"%s\".\n",
+				uaMobile)
+		}
 
-	// Test if has values
-	if !hasValues {
-		t.Errorf("Expected propery \"%s\" to have values.\n", propertyName)
+		// Check results
+		propertyName := "IsMobile"
+		hasValues, err := results.HasValues(propertyName)
+		if err != nil {
+			t.Error("Failed to perform 'HasValues' on results.")
+		}
+
+		// Test if has values
+		if !hasValues {
+			t.Errorf("Expected propery \"%s\" to have values.\n", propertyName)
+		}
 	}
+	// Execute the test with different performance profiles
+	testDifferentPerformanceProfiles(testFunc, t)
 }
 
 // This tests HasValues reports correctly when no
 // values is expected. This also test the correct
 // message being returned.
 func TestHasNoValues(t *testing.T) {
-	// Create results hash
-	results, err := NewResultsHash(manager, 1, 0)
-	if err != nil {
-		t.Error("Failed to create results hash object.")
-	}
-
-	// Free results hash at the end
-	defer func() {
-		err = results.Free()
+	var testFunc TestFunc = func(manager *ResourceManager, t *testing.T) {
+		// Create results hash
+		results, err := NewResultsHash(manager, 1, 0)
 		if err != nil {
-			t.Error("Failed to free results hash object.")
+			t.Error("Failed to create results hash object.")
 		}
-	}()
 
-	if err := results.MatchUserAgent(""); err != nil {
-		t.Errorf("Failed to perform detection on User-Agent \"%s\".\n",
-			uaMobile)
-	}
+		// Free results hash at the end
+		defer func() {
+			err = results.Free()
+			if err != nil {
+				t.Error("Failed to free results hash object.")
+			}
+		}()
 
-	// Check results
-	propertyName := "IsMobile"
-	hasValues, err := results.HasValues(propertyName)
-	if err != nil {
-		t.Error("Failed to perform 'HasValues' on results.")
-	}
+		if err := results.MatchUserAgent(""); err != nil {
+			t.Errorf("Failed to perform detection on User-Agent \"%s\".\n",
+				uaMobile)
+		}
 
-	// Test if has values
-	if hasValues {
-		t.Errorf("Expected propery \"%s\" to have no values.\n", propertyName)
-	}
+		// Check results
+		propertyName := "IsMobile"
+		hasValues, err := results.HasValues(propertyName)
+		if err != nil {
+			t.Error("Failed to perform 'HasValues' on results.")
+		}
 
-	// Test values string
-	value, _, err := results.ValuesString(propertyName, 50, ",")
-	if err != nil {
-		t.Error("Failed to get values string.")
-	}
+		// Test if has values
+		if hasValues {
+			t.Errorf("Expected propery \"%s\" to have no values.\n", propertyName)
+		}
 
-	// Expected no value
-	if value != "" {
-		t.Error("Expected value to an empty string")
-	}
+		// Test values string
+		value, _, err := results.ValuesString(propertyName, 50, ",")
+		if err != nil {
+			t.Error("Failed to get values string.")
+		}
 
-	// Test no values message
-	message, err := results.NoValueReasonMessage(propertyName)
-	if err != nil {
-		t.Error("Failed to get no value message.")
-	}
+		// Expected no value
+		if value != "" {
+			t.Error("Expected value to an empty string")
+		}
 
-	// Expected certain message
-	if !strings.Contains(message, "No matching profiles could be found") {
-		t.Error("In correct values message.")
+		// Test no values message
+		message, err := results.NoValueReasonMessage(propertyName)
+		if err != nil {
+			t.Error("Failed to get no value message.")
+		}
+
+		// Expected certain message
+		if !strings.Contains(message, "No matching profiles could be found") {
+			t.Error("In correct values message.")
+		}
 	}
+	// Execute the test with different performance profiles
+	testDifferentPerformanceProfiles(testFunc, t)
 }
 
 // This tests a list of values are returned correctly
@@ -299,91 +298,99 @@ func TestValues(t *testing.T) {
 // This tests ValuesString to be returned correctly
 // for a property including input.
 func TestValuesString(t *testing.T) {
-	// Expected output
-	data := struct {
-		property string
-		expected string
-	}{"IsMobile", "true"}
+	var testFunc TestFunc = func(manager *ResourceManager, t *testing.T) {
+		// Expected output
+		data := struct {
+			property string
+			expected string
+		}{"IsMobile", "true"}
 
-	// Create results hash
-	results, err := NewResultsHash(manager, 1, 0)
-	if err != nil {
-		t.Error("Failed to create results hash object.")
-	}
-
-	// Free results hash at the end
-	defer func() {
-		err = results.Free()
+		// Create results hash
+		results, err := NewResultsHash(manager, 1, 0)
 		if err != nil {
-			t.Error("Failed to free results hash object.")
+			t.Error("Failed to create results hash object.")
 		}
-	}()
 
-	if err := results.MatchUserAgent(uaMobile); err != nil {
-		t.Errorf("Failed to perform detection on User-Agent \"%s\".\n",
-			uaMobile)
-	}
+		// Free results hash at the end
+		defer func() {
+			err = results.Free()
+			if err != nil {
+				t.Error("Failed to free results hash object.")
+			}
+		}()
 
-	// Get values
-	value, _, err := results.ValuesString(data.property, 50, ",")
-	if err != nil {
-		t.Errorf("Failed to get values string for User-Agent \"%s\".\n",
-			uaMobile)
+		if err := results.MatchUserAgent(uaMobile); err != nil {
+			t.Errorf("Failed to perform detection on User-Agent \"%s\".\n",
+				uaMobile)
+		}
+
+		// Get values
+		value, _, err := results.ValuesString(data.property, 50, ",")
+		if err != nil {
+			t.Errorf("Failed to get values string for User-Agent \"%s\".\n",
+				uaMobile)
+		}
+		// Check if detection is correct
+		if !strings.EqualFold(value, data.expected) {
+			t.Errorf("Expected \"%s\" but get \"%s\"\n.",
+				data.expected,
+				value,
+			)
+		}
 	}
-	// Check if detection is correct
-	if !strings.EqualFold(value, data.expected) {
-		t.Errorf("Expected \"%s\" but get \"%s\"\n.",
-			data.expected,
-			value,
-		)
-	}
+	// Execute the test with different performance profiles
+	testDifferentPerformanceProfiles(testFunc, t)
 }
 
 // This tests ValuesString to be returned correctly
 // for a property including input.
 func TestValuesStringInsufficientSize(t *testing.T) {
-	// Expected output
-	data := struct {
-		property string
-		expected uint64
-	}{"IsMobile", uint64(len("true"))}
+	var testFunc TestFunc = func(manager *ResourceManager, t *testing.T) {
+		// Expected output
+		data := struct {
+			property string
+			expected uint64
+		}{"IsMobile", uint64(len("true"))}
 
-	// Create results hash
-	results, err := NewResultsHash(manager, 1, 0)
-	if err != nil {
-		t.Error("Failed to create results hash object.")
-	}
-
-	// Free results hash at the end
-	defer func() {
-		err = results.Free()
+		// Create results hash
+		results, err := NewResultsHash(manager, 1, 0)
 		if err != nil {
-			t.Error("Failed to free results hash object.")
+			t.Error("Failed to create results hash object.")
 		}
-	}()
 
-	if err := results.MatchUserAgent(uaMobile); err != nil {
-		t.Errorf("Failed to perform detection on User-Agent \"%s\".\n",
-			uaMobile)
-	}
+		// Free results hash at the end
+		defer func() {
+			err = results.Free()
+			if err != nil {
+				t.Error("Failed to free results hash object.")
+			}
+		}()
 
-	// Get values
-	value, actualSize, err := results.ValuesString(data.property, 1, ",")
-	if err != nil {
-		t.Errorf("Failed to get values string for User-Agent \"%s\".\n",
-			uaMobile)
-	}
+		if err := results.MatchUserAgent(uaMobile); err != nil {
+			t.Errorf("Failed to perform detection on User-Agent \"%s\".\n",
+				uaMobile)
+		}
 
-	// Make sure actualSize is correct
-	if actualSize != data.expected {
-		t.Errorf("Expected actual size to be %d but get %d.\n",
-			data.expected,
-			actualSize)
-	}
+		// Get values
+		value, actualSize, err := results.ValuesString(data.property, 1, ",")
+		if err != nil {
+			t.Errorf("Failed to get values string for User-Agent \"%s\".\n",
+				uaMobile)
+		}
 
-	// Make sure value is emtpy
-	if value != "" {
-		t.Error("Expected value to be an empty string since there wasn't " +
-			"enough space allocated.")
+		// Make sure actualSize is correct
+		if actualSize != data.expected {
+			t.Errorf("Expected actual size to be %d but get %d.\n",
+				data.expected,
+				actualSize)
+		}
+
+		// Make sure value is emtpy
+		if value != "" {
+			t.Error("Expected value to be an empty string since there wasn't " +
+				"enough space allocated.")
+		}
 	}
+	// Execute the test with different performance profiles
+	testDifferentPerformanceProfiles(testFunc, t)
 }
