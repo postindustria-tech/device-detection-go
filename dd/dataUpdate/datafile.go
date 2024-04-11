ad97099088d00306b5883589c525cb42cca11a7b
AntonYPost <anton.yushchuk@postindustria.com>
2024-04-11 13:56:33 +0300

    log, url, fixes for datafile update

--- dd/dataUpdate/datafile.go
+++ dd/dataUpdate/datafile.go
@@ -1,37 +1,92 @@
 package main
 
 import (
+	"fmt"
+	"net/url"
+	"os"
 	"time"
 )
 
 type DataFile struct {
-	Identifier                     string // Name of the datafile
-	TempPath                       string // Temporary datafile path
-	Path                           string // Path to the datafile
-	AutoUpdate                     bool   // Whether to automatically update the datafile
-	PollingInterval                int    // How often to poll for updates to the datafile (minutes)
-	UpdateTimeMaximumRandomisation int    // Maximum randomisation offset in seconds to polling time interval
-	UpdateOnStart                  bool   // Whether to update the datafile as soon as it is initialised
-	DataUpdateUrl                  string // TODO: Should be object?
+	Identifier                     string    // Name of the datafile
+	TempPath                       string    // Temporary datafile path
+	Path                           string    // Path to the datafile
+	AutoUpdate                     bool      // Whether to automatically update the datafile
+	PollingInterval                int       // How often to poll for updates to the datafile (minutes)
+	UpdateTimeMaximumRandomisation int       // Maximum randomisation offset in seconds to polling time interval
+	UpdateOnStart                  bool      // Whether to update the datafile as soon as it is initialised
+	UpdateURLParams                URLParams // URL params for the update
 	DataPublishedDateTime          time.Time
 	VerifyIfModifiedSince          bool
 	LastUpdateTryTime              time.Time
 	Updating                       bool
 }
 
-func (df *DataFile) urlFormatter() string {
-	// by default just return the url
-	return df.DataUpdateUrl
+//TODO constructor for DataFile?
+
+type URLParams struct {
+	url         string // URL path to  51Degrees Distributor service
+	LicenseKeys string /*
+		Pipe-separated list of 51Degrees License Keys.
+		You must have a valid License Key for the data file that you are trying to download.
+	*/
+	product string /*
+	  The 51Degrees 'product' that the data file is part of.
+	   Accepts "Premium", "Enterprise", "Lite", "TAC", "V4Free", "V4Enterprise", "V4TAC" values.
+	*/
+	urlType string /*
+	   The type of data file to download.
+	   Accepts "BinaryV3", "BinaryV3Uncompressed", "BinaryV32", "BinaryV32Uncompressed",
+	   "BinaryV32UAT", "TrieV32", "HashTrieV34", "CSV", "HashV41" values.
+	*/
 }
 
-// func (df *DataFile) getPublishedDate() time.Time {
-// 	var publishedDate = df.DataPublishedDateTime
-// 	if publishedDate.IsZero() {
-// 		fileInfo, err := os.Stat(df.Path)
-// 		if err != nil {
+func NewURLParams(urlString, LicenseKeys, product, urlType string) (URLParams, error) {
+	_, err := url.ParseRequestURI(urlString)
+	if err != nil {
+		return URLParams{}, fmt.Errorf("invalid url: %w", err)
+	}
+	switch product {
+	case "Premium", "Enterprise", "Lite", "TAC", "V4Free", "V4Enterprise", "V4TAC":
+	default:
+		return URLParams{}, fmt.Errorf("invalid product value")
+	}
+	switch urlType {
+	case "BinaryV3", "BinaryV3Uncompressed", "BinaryV32", "BinaryV32Uncompressed", "BinaryV32UAT", "TrieV32", "HashTrieV34", "CSV", "HashV41":
+	default:
+		return URLParams{}, fmt.Errorf("invalid type value")
 
-// 		}
-// 		publishedDate = fileInfo.ModTime()
-// 	}
-// 	return publishedDate
-// }
+	}
+	return URLParams{url: urlString, LicenseKeys: LicenseKeys, product: product, urlType: urlType}, nil
+}
+
+func (param *URLParams) ToString() string {
+	u, _ := url.Parse(param.url)
+
+	query := u.Query()
+	query.Set("LicenseKeys", param.LicenseKeys)
+	query.Set("Product", param.product)
+	query.Set("Type", param.urlType)
+
+	u.RawQuery = query.Encode()
+	return u.String()
+}
+
+func (df *DataFile) urlFormatter() string {
+	return df.UpdateURLParams.ToString()
+}
+
+func (df *DataFile) getPublishedDate() (time.Time, error) {
+	var publishedDate = df.DataPublishedDateTime
+	if publishedDate.IsZero() {
+		if df.Path == "" {
+			return time.Time{}, fmt.Errorf("no publish date exists")
+		}
+		fileInfo, err := os.Stat(df.Path)
+		if err != nil {
+			return time.Time{}, fmt.Errorf("can't get publish date: %w", err)
+		}
+		publishedDate = fileInfo.ModTime()
+	}
+	return publishedDate, nil
+}
--- dd/dataUpdate/datafile_update_service.go
+++ dd/dataUpdate/datafile_update_service.go
@@ -3,8 +3,10 @@ package main
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"errors"
+	"fmt"
 	"io"
-	"log"
+	"log/slog"
 	"math"
 	"math/rand"
 	"net/http"
@@ -16,105 +18,122 @@ type DataFileUpdateService struct {
 	Updating bool
 }
 
-func (service *DataFileUpdateService) UpdateDataFile(df DataFile) {
+func (service *DataFileUpdateService) UpdateDataFile(df DataFile) error {
 	if service.Updating {
-		//TODO log message
-		return
+		slog.Warn("Updating data is already in progress")
+		return nil
 	}
 	service.Updating = true
 	defer func() {
+		if r := recover(); r != nil {
+			slog.Error("Recovered: ", r)
+		}
 		service.Updating = false
 		service.CheckNextUpdate(df)
 	}()
 
 	req, err := http.NewRequest("GET", df.urlFormatter(), nil)
 	if err != nil {
-		//TODO: err
-		log.Fatal(err)
+		return fmt.Errorf("error creating request: %w", err)
 	}
-	if df.VerifyIfModifiedSince {
-		req.Header.Set("If-Modified-Since", df.DataPublishedDateTime.Format(http.TimeFormat))
+	if df.VerifyIfModifiedSince && df.Path != "" {
+		publishDate, err := df.getPublishedDate()
+		if err == nil {
+			req.Header.Set("If-Modified-Since", publishDate.Format(http.TimeFormat))
+		} else {
+			slog.Warn("Couldn't verify modified since")
+		}
 	}
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		//TODO: err
-		log.Fatal(err)
+		return fmt.Errorf("error sending request: %w", err)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
 		switch resp.StatusCode {
 		case http.StatusTooManyRequests:
+			return fmt.Errorf("too many requests to %s", df.urlFormatter())
 		case http.StatusNotModified:
+			return fmt.Errorf("no data update available at %s", df.urlFormatter())
 		case http.StatusForbidden:
+			return fmt.Errorf("access denied to %s", df.urlFormatter())
 		default:
+			return fmt.Errorf("response error from %s: %s", df.urlFormatter(), resp.Status)
 		}
-		//TODO: err
-		return
 	}
 
 	contentMD5 := resp.Header.Get("Content-MD5")
-	if contentMD5 != "" && !service.VerifyMD5(resp.Body, contentMD5) {
-		//TODO: err if VerifyMD5 false
-		return
+	if contentMD5 != "" {
+		verify, err := service.VerifyMD5(resp.Body, contentMD5)
+		if err != nil {
+			return fmt.Errorf("error verifying MD5: %w", err)
+		}
+		if !verify {
+			errMsg := fmt.Sprintf("response header MD5 '%s' does not match file's", contentMD5)
+			return errors.New(errMsg)
+		}
 	}
 
-	service.SaveDataFile(df, resp.Body)
+	err = service.SaveDataFile(df, resp.Body)
+	if err != nil {
+		return fmt.Errorf("error saving datafile: %w", err)
+	}
+	return nil
 }
 
-func (service *DataFileUpdateService) SaveDataFile(df DataFile, data io.ReadCloser) {
+func (service *DataFileUpdateService) SaveDataFile(df DataFile, data io.ReadCloser) error {
 	err := os.MkdirAll(df.TempPath, os.ModePerm)
 	if err != nil {
-		log.Fatal(err)
-		//TODO: err
+		return fmt.Errorf("error creating temp folder: %w", err)
 	}
 
 	tempFilePath := df.TempPath + "/" + df.Identifier
 	tempFile, err := os.Create(tempFilePath)
 	if err != nil {
-		log.Fatal(err)
-		//TODO: err
+		return fmt.Errorf("error creating file: %w", err)
 	}
 	defer tempFile.Close()
 
 	_, err = tempFile.ReadFrom(data)
 	if err != nil {
-		log.Fatal(err)
-		//TODO: err
+		return fmt.Errorf("error writing file: %w", err)
 	}
 
 	err = os.Rename(tempFilePath, df.Path+"/"+df.Identifier)
 	if err != nil {
-		log.Fatal(err)
-		//TODO: err
+		return fmt.Errorf("error moving file: %w", err)
 	}
+
+	return nil
 }
 
-func (service *DataFileUpdateService) CheckNextUpdate(df DataFile) {
+func (service *DataFileUpdateService) CheckNextUpdate(df DataFile) error {
 	if df.AutoUpdate {
 		randomOffset := rand.Intn(abs(df.UpdateTimeMaximumRandomisation) + 1)
 		interval := 60*abs(df.PollingInterval) + randomOffset
 
 		time.AfterFunc(time.Second*time.Duration(interval), func() { service.UpdateDataFile(df) })
+		//TODO error return
 	}
+	return nil
 }
 
-func (service *DataFileUpdateService) RegisterDataFile(df DataFile) {
+func (service *DataFileUpdateService) RegisterDataFile(df DataFile) error {
 	if df.UpdateOnStart {
-		service.UpdateDataFile(df)
+		return service.UpdateDataFile(df)
 	} else {
-		service.CheckNextUpdate(df)
+		return service.CheckNextUpdate(df)
 	}
 }
 
-func (service *DataFileUpdateService) VerifyMD5(data io.ReadCloser, md5Hash string) bool {
+func (service *DataFileUpdateService) VerifyMD5(data io.ReadCloser, md5Hash string) (bool, error) {
 	h := md5.New()
 	if _, err := io.Copy(h, data); err != nil {
-		//TODO: err
-		log.Fatal(err)
+		return false, fmt.Errorf("error copying file: %w", err)
 	}
-	return hex.EncodeToString(h.Sum(nil)) == md5Hash
+	return hex.EncodeToString(h.Sum(nil)) == md5Hash, nil
 }
 
 func abs(x int) int {
