ccda58b952c1211f2cf5c6dc0b64e6716336741a
Marin Miletic <mrnmiletic@gmail.com>
2024-04-11 22:07:43 +0200

    feat: mvp data file pulling

---
+++ pipeline/file_pulling.go
@@ -0,0 +1,152 @@
+package pipeline
+
+import (
+	"bytes"
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+)
+
+// scheduleFilePulling schedules the file pulling
+func (p *Pipeline) scheduleFilePulling() {
+	// recover from panic
+	// if panic occurs, we will log the error and restart the file pulling
+	defer func() {
+		if r := recover(); r != nil {
+			p.logger.Printf("error occurred when pulling data: %v", r)
+			go p.scheduleFilePulling()
+		}
+	}()
+
+	nextIterationInMs := p.dataFilePullEveryMs
+
+	isFirstRun := true
+
+	for {
+		// if this is the first run, we don't need to wait
+		if isFirstRun {
+			isFirstRun = false
+		} else {
+			<-time.After(time.Duration(nextIterationInMs) * time.Millisecond)
+		}
+
+		p.logger.Printf("Pulling data from %s", p.dataFileUrl)
+
+		fileResponse, err := doDataFileRequest(p.dataFileUrl)
+		if err != nil {
+			p.logger.Printf("failed to pull data file: %v", err)
+			if fileResponse.retryAfter > 0 {
+				p.logger.Printf("received retry-after, retrying after %d seconds", fileResponse.retryAfter)
+
+				// retry after the specified time
+				nextIterationInMs = fileResponse.retryAfter * 1000
+			} else {
+				p.logger.Printf("retrying after 1 second")
+				// retry after 1 second, since we have unhandled error
+				// this can happen from network stutter or something else
+				nextIterationInMs = 1000
+			}
+			continue
+		}
+
+		p.logger.Printf("data file pulled successfully")
+
+		// write to dataFile
+		err = os.WriteFile(p.dataFile, fileResponse.buffer.Bytes(), 0644)
+		if err != nil {
+			p.logger.Printf("failed to write data file: %v", err)
+			// retry after 1 second, since we have unhandled error
+			// this can happen from disk write error or something else
+			nextIterationInMs = 1000
+			continue
+		}
+		p.logger.Printf("data file written successfully: %d bytes", fileResponse.buffer.Len())
+
+		err = p.manager.ReloadFromOriginalFile()
+		p.logger.Printf("data file reloaded successfully")
+		if err != nil {
+			p.logger.Printf("failed to reload data file: %v", err)
+			// retry after 1 second, since we have unhandled error
+			// this can happen from reload error or something else
+			nextIterationInMs = 1000
+			continue
+		}
+		// reset nextIterationInMs
+		nextIterationInMs = p.dataFilePullEveryMs
+	}
+}
+
+type FileResponse struct {
+	buffer     bytes.Buffer
+	retryAfter int
+}
+
+// doDataFileRequest performs the data file request
+func doDataFileRequest(url string) (*FileResponse, error) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	//handle retry after
+	if resp.StatusCode == http.StatusTooManyRequests {
+		retryAfter := resp.Header.Get("Retry-After")
+		retryAfterSeconds, err := strconv.Atoi(retryAfter)
+		if err != nil || len(retryAfter) == 0 {
+			return nil, fmt.Errorf("failed to parse Retry-After header: %v", err)
+		}
+
+		return &FileResponse{
+			retryAfter: retryAfterSeconds,
+		}, fmt.Errorf("received 429, retrying after %d seconds", retryAfterSeconds)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to pull data file: %s", resp.Status)
+	}
+
+	contentMD5 := resp.Header.Get("Content-MD5")
+
+	fmt.Printf("Content-MD5: %s\n", contentMD5)
+
+	isValid, err := validateMd5(resp.Body, contentMD5)
+	if err != nil {
+		return nil, fmt.Errorf("failed to validate MD5: %v", err)
+	}
+
+	if !isValid {
+		return nil, fmt.Errorf("MD5 validation failed")
+	}
+
+	var buffer bytes.Buffer
+	_, err = io.Copy(&buffer, resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read response body: %v", err)
+	}
+
+	return &FileResponse{
+		buffer:     buffer,
+		retryAfter: 0,
+	}, nil
+}
+
+func validateMd5(body io.ReadCloser, hash string) (bool, error) {
+	h := md5.New()
+	_, err := io.Copy(h, body)
+	if err != nil {
+		return false, err
+	}
+
+	return hex.EncodeToString(h.Sum(nil)) == hash, nil
+}
---
+++ pipeline/pipeline.go
@@ -0,0 +1,104 @@
+package pipeline
+
+import "github.com/51Degrees/device-detection-go/v4/dd"
+
+type Pipeline struct {
+	logger                        logWrapper
+	dataFile                      string
+	licenceKey                    string
+	createTempDataCopy            bool
+	dataUpdateOnStartEnabled      bool
+	dataFileUrl                   string
+	dataFilePullEveryMs           int
+	isScheduledFilePullingEnabled bool
+	loggerEnabled                 bool
+	manager                       *dd.ResourceManager
+	config                        *dd.ConfigHash
+}
+
+// Run starts the pipeline
+func (p *Pipeline) Run() error {
+	dataFile, err := dd.GetFilePath(".", []string{p.dataFile})
+	if err != nil {
+		return err
+	}
+	p.dataFile = dataFile
+
+	// TODO: what are properties of dd.ResourceManager?
+	err = dd.InitManagerFromFile(p.manager, *p.config, "", p.dataFile)
+	if err != nil {
+		return err
+	}
+
+	if p.isScheduledFilePullingEnabled {
+		go p.scheduleFilePulling()
+	}
+
+	return nil
+}
+
+type PipelineOptions func(cfg *Pipeline) error
+
+// what should this do? c# has it and it's used in the example.
+func UseOnPremise(dataFile, licenceKey string, createTempDataCopy bool) PipelineOptions {
+
+	return func(cfg *Pipeline) error {
+		cfg.dataFile = dataFile
+		cfg.licenceKey = licenceKey
+		cfg.createTempDataCopy = createTempDataCopy
+
+		return nil
+	}
+}
+
+// WithDataUpdateUrl sets the URL to pull data from and the interval in milliseconds
+func WithDataUpdateUrl(url string, everyMs int) PipelineOptions {
+	return func(cfg *Pipeline) error {
+		cfg.dataFileUrl = url
+		cfg.dataFilePullEveryMs = everyMs
+		cfg.isScheduledFilePullingEnabled = true
+
+		return nil
+	}
+}
+
+// ToggleLogger enables or disables the logger
+func ToggleLogger(enabled bool) PipelineOptions {
+	return func(cfg *Pipeline) error {
+		cfg.logger.enabled = enabled
+		return nil
+	}
+}
+
+// WithCustomLogger sets a custom logger
+func WithCustomLogger(logger LogWriter) PipelineOptions {
+	return func(cfg *Pipeline) error {
+		cfg.logger = logWrapper{
+			enabled: true,
+			logger:  logger,
+		}
+
+		return nil
+	}
+
+}
+
+func New(manager *dd.ResourceManager, config *dd.ConfigHash, opts ...PipelineOptions) (*Pipeline, error) {
+	pl := &Pipeline{
+		logger: logWrapper{
+			logger:  DefaultLogger,
+			enabled: true,
+		},
+		manager: manager,
+		config:  config,
+	}
+
+	for _, opt := range opts {
+		err := opt(pl)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return pl, nil
+}
